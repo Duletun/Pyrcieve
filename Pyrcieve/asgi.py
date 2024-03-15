86c99758ecd8109bc037092e5cafc2f6cfc93ecc
Duletun <duletun@gmail.com>
2024-03-15 14:09:49 +0300

    Rename commit

--- Pyrcieve/asgi.py
+++ Pyrcieve/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for doliDjango project.
+ASGI config for Pyrcieve project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'doliDjango.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Pyrcieve.settings')
 
 application = get_asgi_application()
