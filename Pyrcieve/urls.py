4699bbde60e3a4df19bfdaa80ef72bc36cc6f09e
Duletun <duletun@gmail.com>
2024-03-19 12:43:40 +0300

    bboard creation commit

--- Pyrcieve/urls.py
+++ Pyrcieve/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from bboard.views import index
 
 urlpatterns = [
+    path('', include('bboard.urls')),
+    path('bboard/', include('bboard.urls')),
     path('admin/', admin.site.urls),
 ]
