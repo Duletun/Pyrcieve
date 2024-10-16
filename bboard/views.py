17a1109ebe69eace7fb0ac5d16d8abc4c81944d8
Duletun <duletun@gmail.com>
2024-10-16 08:12:08 +0300

    small comment

--- bboard/views.py
+++ bboard/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render
 from .models import Bb
-
+# просмотр модели
 def index(request):
     bbs = Bb.objects.all()
     return render(request, 'index.html', {'bbs': bbs})
