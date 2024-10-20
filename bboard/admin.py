b2963bbc365eed058b6cc4562dfcdc6a7a138f3f
Duletun <duletun@gmail.com>
2024-10-20 22:05:15 +0300

    bbadmin added

--- bboard/admin.py
+++ bboard/admin.py
@@ -3,4 +3,10 @@ from django.contrib import admin
 # Register your models here.
 from django.contrib import admin
 from .models import Bb
-admin.site.register(Bb)
+
+class BbAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'price', 'published')
+    list_display_links = ('title', 'content')
+    search_fields = ('title', 'content')
+
+admin.site.register(Bb, BbAdmin)
